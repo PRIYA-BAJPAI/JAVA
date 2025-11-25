067d66d59ce4d9c31a1f012a97a83a7a1c2c2631
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-25 16:10:33 +0530

    collection

---
+++ lecture10/collections.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class collections {
+    public static void main(String[] args) {
+        ArrayList<Integer>list=new ArrayList<>();
+        list.add(5);
+        list.add(10);
+        list.add(1,30);
+        System.out.println(list);
+    }
+}
+//LinkedList<Integer>list=new LinkedList<>();
---
+++ lecture10/extra.java
@@ -0,0 +1,7 @@
+package lecture10;
+
+public class extra {
+    public static void main(String[] args) {
+        int first=list.get(0);
+    }
+}
