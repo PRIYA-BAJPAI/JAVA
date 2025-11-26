312d9a4d9e1d268660f98bd4abeb35851494a631
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-26 16:13:28 +0530

    target sum

--- lecture11/sum.java
+++ lecture11/sum.java
@@ -2,7 +2,16 @@ package lecture11;
 import java.util.*;
 public class sum {
     public static void main(String[] args) {
-        int arr[]={2,3,5,7};
-         
-    }
+        int arr[]={2,3,4,5};
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int target=9;
+        for(int i=0;i<arr.length;i++){
+            int diff=target-arr[i];
+            if(map.containsKey(diff)){
+                System.out.println(map.get(diff)+" "+i);
+                break;
+            }
+            map.put(arr[i],i);
+        }
+        }
 }
