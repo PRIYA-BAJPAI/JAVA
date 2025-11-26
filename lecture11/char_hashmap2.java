a064a47a758e30f472aa4ba8a911bb0f841fbeae
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-26 15:55:24 +0530

    freq1

---
+++ lecture11/char_hashmap2.java
@@ -0,0 +1,20 @@
+//print char that occured once
+package lecture11;
+import java.util.*;
+
+public class char_hashmap2 {
+      public static void main(String[] args) {
+        String str="aabcad";
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(char ch : str.toCharArray()){
+            map.put(ch,map.getOrDefault(ch, 0)+1);
+        }
+        System.out.println(map);
+        for(char ch:str.toCharArray()){
+            if(map.get(ch)==1){
+                System.out.println(ch);
+                
+            }
+        }
+    }
+}
