4096106d06a940f5931005f90d8b420b5b1785c4
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-26 16:20:20 +0530

    update

--- lecture11/sum.java
+++ lecture11/sum.java
@@ -6,12 +6,12 @@ public class sum {
         HashMap<Integer,Integer>map=new HashMap<>();
         int target=9;
         for(int i=0;i<arr.length;i++){
-            int diff=target-arr[i];
-            if(map.containsKey(diff)){
-                System.out.println(map.get(diff)+" "+i);
+            int diff=target-arr[i]; //difference find karenge
+            if(map.containsKey(diff)){ //agar diff ki value map me hai to loop chalega 
+                System.out.println(map.get(diff)+" "+i); 
                 break;
             }
-            map.put(arr[i],i);
+            map.put(arr[i],i); //otherwise map map create hoga 
         }
         }
 }
