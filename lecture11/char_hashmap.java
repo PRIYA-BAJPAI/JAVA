575ffc946cac969a69d628ca76b4bdc0881baf4a
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-26 15:54:34 +0530

    char

---
+++ lecture11/char_hashmap.java
@@ -0,0 +1,18 @@
+//print first char that occured once
+import java.util.*;
+public class char_hashmap {
+    public static void main(String[] args) {
+        String str="aabcad";
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(char ch : str.toCharArray()){
+            map.put(ch,map.getOrDefault(ch, 0)+1);
+        }
+        System.out.println(map);
+        for(char ch:str.toCharArray()){
+            if(map.get(ch)==1){
+                System.out.println(ch);
+                break;
+            }
+        }
+    }
+}
