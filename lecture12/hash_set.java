b915cbe98057e6da10e679dc299cdeeaed719f76
PRIYA-BAJPAI <priyabajpai1506@gmail.com>
2025-11-28 13:08:39 +0530

    set

---
+++ lecture12/hash_set.java
@@ -0,0 +1,11 @@
+package lecture12;
+import java.util.HashSet;
+public class hash_set {
+    public static void main(String[] args) {
+        HashSet<Integer>set=new HashSet<>();
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        System.out.println(set);
+    }
+}
